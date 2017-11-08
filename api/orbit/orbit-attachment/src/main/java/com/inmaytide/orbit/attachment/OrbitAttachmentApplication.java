d7a4a6c784eb7505d25377a839a0d610eed0b179
TOBLERONE。 <inmaytide@gmail.com>
2017-11-08 22:58:58 +0800

    Debug attachment service

--- api/orbit/orbit-attachment/src/main/java/com/inmaytide/orbit/attachment/OrbitAttachmentApplication.java
+++ api/orbit/orbit-attachment/src/main/java/com/inmaytide/orbit/attachment/OrbitAttachmentApplication.java
@@ -1,14 +1,23 @@
 package com.inmaytide.orbit.attachment;
 
 import com.inmaytide.orbit.attachment.handler.AttachmentHandler;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mybatis.repository.config.EnableMybatisRepositories;
+import org.springframework.data.mybatis.support.SqlSessionFactoryBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.server.HandlerStrategies;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
@@ -16,13 +25,22 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.notFound;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
 
-@SpringBootApplication
-@EnableMybatisRepositories
 @EnableWebFlux
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableMybatisRepositories
 @EnableTransactionManagement
-@PropertySource(ignoreResourceNotFound = true, value = "classpath:orbit-attachment.yaml")
 public class OrbitAttachmentApplication {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(HikariDataSource dataSource) {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        return bean;
+    }
 
     @Bean
     public RouterFunction<?> routers(AttachmentHandler handler) {
@@ -30,7 +48,15 @@ public class OrbitAttachmentApplication {
                 .and(route(GET("/{id}"), handler::download))
                 .and(route(DELETE("/{ids}"), handler::remove))
                 .and(route(DELETE("/"), handler::removeByBelong));
-        return nest(path("/attachments"), routers);
+        return nest(path("/attachments"), routers)
+                .andOther(route(RequestPredicates.all(), (request) -> status(HttpStatus.NOT_FOUND).body(Mono.just("123") , String.class)));
+    }
+
+    @Bean
+    public HttpHandler httpHandler(RouterFunction routers) {
+        return WebHttpHandlerBuilder
+                .webHandler(RouterFunctions.toWebHandler(routers, HandlerStrategies.empty().build()))
+                .build();
     }
 
     public static void main(String[] args) {
