3f41cb12b792187d1b68ddd478da80372b5f8422
inmaytde <inmaytide@gmail.com>
2018-01-26 14:01:23 +0800

    configurations center

--- backend/orbit/orbit-authorization/src/main/java/com/inmaytide/orbit/auth/AuthorizationApplication.java
+++ backend/orbit/orbit-authorization/src/main/java/com/inmaytide/orbit/auth/AuthorizationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableOAuth2Client
+@EnableFeignClients
 public class AuthorizationApplication {
 
     public static void main(String... args) {
