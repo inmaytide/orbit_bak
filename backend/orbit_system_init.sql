f8449b9bfa05f8bb3d3e4a221e07960376259153
TOBLERONE。 <inmaytide@gmail.com>
2018-07-31 22:03:10 +0800

    create sys_menu table

--- backend/orbit_system_init.sql
+++ backend/orbit_system_init.sql
@@ -22,3 +22,20 @@ create table public.sys_user (
 
 insert into public.sys_user(id, name, username, password, status, create_time, creator, version)
     values(423441402547015680, 'administrator', 'admin', '$2a$10$8CDOiERjl1Y08dA.IbkaZuBYmrlkCEi.9sbrVaGYyeYKoQe87aYNi', 1, now(), 423441402547015680, 0);
+
+
+create table public.sys_menu (
+  id bigint primary key,
+  code varchar(64) not null unique,
+  name varchar(64) not null,
+  method varchar(8),
+  url varchar(256),
+  description varchar(512),
+  parent bigint not null default 0,
+  seq_order int,
+  create_time timestamp,
+  creator bigint,
+  update_time timestamp,
+  updater bigint,
+  version int
+)
