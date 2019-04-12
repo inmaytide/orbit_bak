f11f248efaba4d2ade56795b967813438cff74fe
luomiao <luomiao@haocang.com>
2019-04-12 09:22:43 +0800

    initial commit

---
+++ authorization/src/main/java/com/inmaytide/orbit/authorization/AuthorizationApplication.java
@@ -0,0 +1,15 @@
+package com.inmaytide.orbit.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+
+}
